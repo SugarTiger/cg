3ce3d77cf4fc6c9b8f8571c880454c17538c2dab
SugarTiger <1029109173@qq.com>
2017-07-31 00:20:11 +0800

    login-middle-height

--- js/login.js
+++ js/login.js
@@ -1,16 +1,16 @@
-function windowHeight() {
-    //获取屏幕宽度
-    var devicHeight = document.documentElement.clientHeight;
-    var middle = document.getElementById('middle');
-    console.log(middle.clientHeight+"::::::::"+devicHeight);
-    if(middle.clientHeight < devicHeight){
-        middle.style.height =  devicHeight +'px';
-    }else{
-        middle.style.height  = 'auto';
-    }
-}
-windowHeight();
-window.onresize = function () {
-    windowSize();
-    windowHeight();
-};
+// function windowHeight() {
+//     //获取屏幕宽度
+//     var devicHeight = document.documentElement.clientHeight;
+//     var middle = document.getElementById('middle');
+//     console.log(middle.clientHeight+"::::::::"+devicHeight);
+//     if(middle.clientHeight < devicHeight){
+//         middle.style.height =  devicHeight +'px';
+//     }else{
+//         middle.style.height  = 'auto';
+//     }
+// }
+// windowHeight();
+// window.onresize = function () {
+//     windowSize();
+//     windowHeight();
+// };
