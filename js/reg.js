cb3e57a1aac40d8de2aef3c33a7c8be68dc6e0f4
SugarTiger <1029109173@qq.com>
2017-07-31 12:08:49 +0800

    code...

--- js/reg.js
+++ js/reg.js
@@ -1,3 +1,15 @@
+var reg = document.querySelector('.reg');
+var codeimg = reg.querySelector('.code_img');
+
+codeimg.onclick = changeVCode;
+function changeVCode(){
+    var imgw = codeimg.clientWidth;
+    var imgh = codeimg.clientHeight;
+    var time = new Date().getTime();
+    codeimg.src="admin/code.php?&n=4&s=20&w="+imgw+"&h="+imgh+"&time="+time;
+}
+changeVCode();
+
 var sub = document.querySelector('.sub');
 sub.onclick = function(event){
     if(event.preventDefault){
@@ -5,7 +17,6 @@ sub.onclick = function(event){
     }else if(event.returnValue){
         event.returnValue = false;
     }
-    var reg = document.querySelector('.reg');
     var user = reg.querySelector('.user').value;
     var pwd = reg.querySelector('.pwd').value;
     var rpwd = reg.querySelector('.rpwd').value;
